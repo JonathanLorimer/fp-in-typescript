104b581c994234da8cc7951c3416104b88754e2c
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-06-12 17:02:20 -0400

    began work on typescript section

---
+++ src/typescript/exercises/1_typeAnnotations.ts
@@ -0,0 +1,8 @@
+// Annotate the argument types and return type
+const addNums = (num1, num2) => num1 + num2
+
+const concatenate = (str1, str2) => str1 + str2
+
+const showNumber = (num) => num.toString()
+
+// Bonus: annotate the function variables
---
+++ src/typescript/pictures/2_interface.ts
@@ -0,0 +1,22 @@
+interface ProgrammingLanguage {
+	name: string
+	functionalSupport: boolean
+	version: number
+}
+
+const javascript: ProgrammingLanguage = {
+	name: "ECMAScript",
+	functionalSupport: true,
+	version: 9,
+}
+const ruby: ProgrammingLanguage = {
+	name: "Ruby",
+	functionalSupport: true,
+	version: 2.6,
+}
+
+const java: ProgrammingLanguage = {
+	name: "java",
+	functionalSupport: false,
+	version: 7,
+}
